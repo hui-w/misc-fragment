f98b83c246304949ede1289530f85ca616cb3c51
hui-w <huiwang@qlike.com>
2016-09-30 16:54:55 +0800

    Implement exporting selected data rows from excel

--- javascript-excel/npm-version/index.js
+++ javascript-excel/npm-version/index.js
@@ -1,14 +1,13 @@
 var XLSX = require('XLSX');
-var workbook = XLSX.readFile('traffic.xlsx');
+var workbook = XLSX.readFile('../AttendanceRecord.xlsx');
 
-/*
+
 var first_sheet_name = workbook.SheetNames[0];
-var address_of_cell = 'A1';
 
 // Get worksheet
 var worksheet = workbook.Sheets[first_sheet_name];
 
-var address_of_cell = 'A2';
+var address_of_cell = 'D26';
 
 // Find desired cell
 var desired_cell = worksheet[address_of_cell];
@@ -17,9 +16,6 @@ var desired_cell = worksheet[address_of_cell];
 var desired_value = desired_cell.v;
 
 console.log(desired_value);
-*/
-
-
 
 /*
 // Iterates through every nonempty of every sheet and dumps values
