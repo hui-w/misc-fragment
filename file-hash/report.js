1b782fe392d254ea22b02bd3c1c75abeab1d4aa3
hui-w <huiwang@qlike.com>
2017-07-21 16:51:03 +0800

    Implement html template system

--- file-hash/report.js
+++ file-hash/report.js
@@ -4,6 +4,31 @@ const fs = require('fs');
 /*
   * Generate the file report
 */
-module.exports = function(fileData) {
-  console.log(fileData);
+module.exports = function(fileData, onComplete) {
+  fs.readFile('./template-report.html', 'utf8', function read(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    const trs = Object.keys(fileData)
+      .map(key => {
+        const row = fileData[key];
+        return `<tr><td>${row.count}</td><td>${row.files}</td></tr>`;
+      })
+      .join('');
+
+    console.log(data);
+
+    const html = data.replace('${trs}', trs);
+
+    fs.writeFile('./report.html', html, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+
+      if (typeof onComplete === 'function') {
+        onComplete();
+      }
+    });
+  });
 };
