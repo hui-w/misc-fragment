a0cffa33f856ffd2923037fa3afda55491555080
hui-w <huiwang@qlike.com>
2018-09-06 14:37:38 +0800

    Updtae toolbar style

--- aes-js/aes-app/src/toolbar.js
+++ aes-js/aes-app/src/toolbar.js
@@ -12,19 +12,26 @@ Toolbar.prototype = {
             class: 'toolbar toolbar-form'
         });
 
+        // Title
+        this.keyInput = this.container.createChild('div', {
+            class: 'toolbar__title-container'
+        }, 'AES');
+
         // Key Input
         this.keyInput = this.container.createChild('div', {
-            class: 'toolbar__input'
-        }, 'Key:').createChild('input', {
-            type: 'password'
-        })
+            class: 'toolbar__input-container'
+        }).createChild('input', {
+            type: 'password',
+            placeholder: "Input Key",
+            class: 'toolbar__key-input'
+        });
 
         // Process Button
         this.processButton = this.container.createChild('div', {
-            class: 'toolbar__button'
+            class: 'toolbar__button-container'
         }).createChild('input', {
             type: 'button',
             value: 'Process'
-        })
+        });
     },
 }
